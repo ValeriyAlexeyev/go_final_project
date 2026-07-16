6083ccce9efeec3790a9b01355d047d8d1803e91
ValeriyAlexeyev <innuaki@yandex.ru>
2026-07-16 17:29:47 +0500

    eigth commit

--- pkg/api/task.go
+++ pkg/api/task.go
@@ -3,24 +3,30 @@ package api
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/ValeriyAlexeyev/go_final_project/pkg/db"
 )
 
+type TasksResponse struct {
+	Tasks []*db.Task `json:"tasks"`
+}
+
 func tasksHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodPost:
-		addTaskHandler(w, r)
-
-	case http.MethodGet:
-		getTaskHandler(w, r)
-
-	case http.MethodPut:
-		updateTaskHandler(w, r)
-
-	default:
-		w.Header().Set(
-			"Allow",
-			http.MethodGet+", "+http.MethodPost+", "+http.MethodPut,
-		)
+	if r.Method != http.MethodGet {
+		w.Header().Set("Allow", http.MethodGet)
 		writeError(w, fmt.Errorf("метод %s не поддерживается", r.Method))
+		return
 	}
+
+	search := r.URL.Query().Get("search")
+
+	tasks, err := db.Tasks(50, search)
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	writeJSON(w, TasksResponse{
+		Tasks: tasks,
+	})
 }
